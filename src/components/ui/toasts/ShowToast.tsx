7b22ed1b01b9792c43bf9fcde871fb731b2e0286
Masheen88 <mattdrummerguy@gmail.com>
2025-02-06 16:29:22 -0600

    Updated toast comments

--- src/components/ui/toasts/ShowToast.tsx
+++ src/components/ui/toasts/ShowToast.tsx
@@ -2,13 +2,14 @@ import { toast, Id } from "react-toastify";
 
 let toastId: Id | null = null; // Track the current toast ID
 
+//display a toast notifiaction info, success, error
 export const showToast = (
   message: string,
   type: "info" | "success" | "error" = "info"
 ) => {
   if (toastId) {
-    toast.update(toastId, { render: message, type, autoClose: 1000 });
+    toast.update(toastId, { render: message, type, autoClose: 1000 }); //closes after 1s
   } else {
-    toastId = toast(message, { type, autoClose: 2000 });
+    toastId = toast(message, { type, autoClose: 2000 }); //closes after 2s
   }
 };
